31c748955050f7fdf148e6e10be56016690fa1a4
Petru <petru@Petrus-MacBook.local>
2020-06-02 14:55:59 +0100

    establishing connection

--- back-end/messenger-server.js
+++ back-end/messenger-server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const socket = require("socket.io");
 
-const messangerServer = app.listen(process.env.PORT || 3001, () => {
+const messangerServer = app.listen(3001, () => {
   console.log(`listening on port 3001`);
 });
 
-app.use(express.static("public"));
+app.use(express.static("front-end"));
 
 const io = socket(messangerServer);
 
 io.on("connection", (socket) => {
-  console.log("Made a socket connection");
+  console.log("Made a socket connection", socket.id);
 });
---
+++ front-end/chat.js
@@ -0,0 +1,3 @@
+//const socket = io.connect("http://localhost:3001");
+
+var socket = io();
